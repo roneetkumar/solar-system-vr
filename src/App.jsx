cbb00dfb8f0dcb4739e337dddcf0863dd78ddf05
Roneet Kumar <roneetparida@gmail.com>
2020-01-14 18:51:48 -0500

    a frame initialised

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>SOLAR</h1>
+            </div>
+        )
+    }
+}
