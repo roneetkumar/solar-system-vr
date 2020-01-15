4852ddedcd4d288e46f9eedd6f493f0389634de7
Roneet Kumar <roneetparida@gmail.com>
2020-01-14 20:17:46 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react'
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App'
+import 'aframe';
+// import 'aframe-animation-component';
+import 'aframe-particle-system-component';
+import 'babel-polyfill';
 
-
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#sceneContainer'));
