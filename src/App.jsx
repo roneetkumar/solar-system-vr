ee46da6ce77ee276196677528078f7d95315068c
Rupinder Virdi <35664372+rockingvirdi96@users.noreply.github.com>
2020-01-14 19:13:36 -0500

    roop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>SOLAR</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>SOLAR VR</h1>
+      </div>
+    );
+  }
 }
